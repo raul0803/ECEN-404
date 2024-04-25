6cf8844ef0600913a666c0c3546c82ef1587f9f4
Raul Cruz <cruzsmiley3@gmail.com>
2024-04-25 10:24:42 -0500

    changed font color of input

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -100,13 +100,13 @@ export default function Example() {
                 <button onClick={closeModal}>close</button>
                 <div></div>
                 <form onSubmit={handleSignIn}>
-                  <input 
-  type="email" 
-  placeholder="Email" 
-  value={email} 
-  onChange={(e) => setEmail(e.target.value)} 
-  required  
-  style={{ color: 'black' }}/>
+                  <input
+                    className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-white dark:border-gray-600dark:hover:bg-gray-700"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
                   <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required className="text-black" />
                   <button
                     type="submit"
